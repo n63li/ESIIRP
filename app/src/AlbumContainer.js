88c76b26911d5eb553b5da9c4d562cb35fe76af3
n63li <n63li@edu.uwaterloo.ca>
2019-01-15 18:54:02 -0500

    Dynamically rendering AlbumCard elements based on number of albums

--- app/src/AlbumContainer.js
+++ app/src/AlbumContainer.js
@@ -36,62 +36,76 @@ class AlbumContainer extends Component{
   }
 
   render(){
+    const albumArray = [];
+    if(this.state.updated){
+      this.state.albums.forEach((album)=>{
+        albumArray.push(
+          <AlbumCard
+            image={album.image}
+            albumName={album.name}
+            releaseYear={album.releaseYear}
+            tracks={album.tracks}
+          />
+        )
+      });
+    }
     return(
       <div className="app-card-list">
-        <AlbumCard
-          image={this.state.updated && this.state.albums[AMSP].image}
-          albumName={this.state.updated && this.state.albums[AMSP].name}
-          releaseYear={this.state.updated && this.state.albums[AMSP].releaseYear}
-          tracks={this.state.updated && this.state.albums[AMSP].tracks}
-        />
-        <AlbumCard
-          image={this.state.updated && this.state.albums[TKOL].image}
-          albumName={this.state.updated && this.state.albums[TKOL].name}
-          releaseYear={this.state.updated && this.state.albums[TKOL].releaseYear}
-          tracks={this.state.updated && this.state.albums[TKOL].tracks}
-        />
-        <AlbumCard
-          image={this.state.updated && this.state.albums[IR].image}
-          albumName={this.state.updated && this.state.albums[IR].name}
-          releaseYear={this.state.updated && this.state.albums[IR].releaseYear}
-          tracks={this.state.updated && this.state.albums[IR].tracks}
-        />
-        <AlbumCard
-          image={this.state.updated && this.state.albums[HTTT].image}
-          albumName={this.state.updated && this.state.albums[HTTT].name}
-          releaseYear={this.state.updated && this.state.albums[HTTT].releaseYear}
-          tracks={this.state.updated && this.state.albums[HTTT].tracks}
-        />
-        <AlbumCard
-          image={this.state.updated && this.state.albums[AMN].image}
-          albumName={this.state.updated && this.state.albums[AMN].name}
-          releaseYear={this.state.updated && this.state.albums[AMN].releaseYear}
-          tracks={this.state.updated && this.state.albums[AMN].tracks}
-        />
-        <AlbumCard
-          image={this.state.updated && this.state.albums[KIDA].image}
-          albumName={this.state.updated && this.state.albums[KIDA].name}
-          releaseYear={this.state.updated && this.state.albums[KIDA].releaseYear}
-          tracks={this.state.updated && this.state.albums[KIDA].tracks}
-        />
-        <AlbumCard
-          image={this.state.updated && this.state.albums[OKC].image}
-          albumName={this.state.updated && this.state.albums[OKC].name}
-          releaseYear={this.state.updated && this.state.albums[OKC].releaseYear}
-          tracks={this.state.updated && this.state.albums[OKC].tracks}
-        />
-        <AlbumCard
-          image={this.state.updated && this.state.albums[TB].image}
-          albumName={this.state.updated && this.state.albums[TB].name}
-          releaseYear={this.state.updated && this.state.albums[TB].releaseYear}
-          tracks={this.state.updated && this.state.albums[TB].tracks}
-        />
-        <AlbumCard
-          image={this.state.updated && this.state.albums[PH].image}
-          albumName={this.state.updated && this.state.albums[PH].name}
-          releaseYear={this.state.updated && this.state.albums[PH].releaseYear}
-          tracks={this.state.updated && this.state.albums[PH].tracks}
-        />
+        {albumArray}
+        {/*<AlbumCard*/}
+          {/*image={this.state.updated && this.state.albums[AMSP].image}*/}
+          {/*albumName={this.state.updated && this.state.albums[AMSP].name}*/}
+          {/*releaseYear={this.state.updated && this.state.albums[AMSP].releaseYear}*/}
+          {/*tracks={this.state.updated && this.state.albums[AMSP].tracks}*/}
+        {/*/>*/}
+        {/*<AlbumCard*/}
+          {/*image={this.state.updated && this.state.albums[TKOL].image}*/}
+          {/*albumName={this.state.updated && this.state.albums[TKOL].name}*/}
+          {/*releaseYear={this.state.updated && this.state.albums[TKOL].releaseYear}*/}
+          {/*tracks={this.state.updated && this.state.albums[TKOL].tracks}*/}
+        {/*/>*/}
+        {/*<AlbumCard*/}
+          {/*image={this.state.updated && this.state.albums[IR].image}*/}
+          {/*albumName={this.state.updated && this.state.albums[IR].name}*/}
+          {/*releaseYear={this.state.updated && this.state.albums[IR].releaseYear}*/}
+          {/*tracks={this.state.updated && this.state.albums[IR].tracks}*/}
+        {/*/>*/}
+        {/*<AlbumCard*/}
+          {/*image={this.state.updated && this.state.albums[HTTT].image}*/}
+          {/*albumName={this.state.updated && this.state.albums[HTTT].name}*/}
+          {/*releaseYear={this.state.updated && this.state.albums[HTTT].releaseYear}*/}
+          {/*tracks={this.state.updated && this.state.albums[HTTT].tracks}*/}
+        {/*/>*/}
+        {/*<AlbumCard*/}
+          {/*image={this.state.updated && this.state.albums[AMN].image}*/}
+          {/*albumName={this.state.updated && this.state.albums[AMN].name}*/}
+          {/*releaseYear={this.state.updated && this.state.albums[AMN].releaseYear}*/}
+          {/*tracks={this.state.updated && this.state.albums[AMN].tracks}*/}
+        {/*/>*/}
+        {/*<AlbumCard*/}
+          {/*image={this.state.updated && this.state.albums[KIDA].image}*/}
+          {/*albumName={this.state.updated && this.state.albums[KIDA].name}*/}
+          {/*releaseYear={this.state.updated && this.state.albums[KIDA].releaseYear}*/}
+          {/*tracks={this.state.updated && this.state.albums[KIDA].tracks}*/}
+        {/*/>*/}
+        {/*<AlbumCard*/}
+          {/*image={this.state.updated && this.state.albums[OKC].image}*/}
+          {/*albumName={this.state.updated && this.state.albums[OKC].name}*/}
+          {/*releaseYear={this.state.updated && this.state.albums[OKC].releaseYear}*/}
+          {/*tracks={this.state.updated && this.state.albums[OKC].tracks}*/}
+        {/*/>*/}
+        {/*<AlbumCard*/}
+          {/*image={this.state.updated && this.state.albums[TB].image}*/}
+          {/*albumName={this.state.updated && this.state.albums[TB].name}*/}
+          {/*releaseYear={this.state.updated && this.state.albums[TB].releaseYear}*/}
+          {/*tracks={this.state.updated && this.state.albums[TB].tracks}*/}
+        {/*/>*/}
+        {/*<AlbumCard*/}
+          {/*image={this.state.updated && this.state.albums[PH].image}*/}
+          {/*albumName={this.state.updated && this.state.albums[PH].name}*/}
+          {/*releaseYear={this.state.updated && this.state.albums[PH].releaseYear}*/}
+          {/*tracks={this.state.updated && this.state.albums[PH].tracks}*/}
+        {/*/>*/}
       </div>
     )
   }
